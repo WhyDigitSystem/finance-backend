6110c8193b4937f75530bb00656bf6d915361c63
dhineshwds <dhinesh@whydigit.in>
2025-02-26 10:25:19 +0530

    fin bug fix

--- src/main/java/com/base/basesetup/repo/RCostInvoiceGnaRepo.java
+++ src/main/java/com/base/basesetup/repo/RCostInvoiceGnaRepo.java
@@ -22,7 +22,7 @@ public interface RCostInvoiceGnaRepo extends JpaRepository<RCostInvoiceGnaVO, Lo
 	@Query(nativeQuery = true, value = "select concat(prefixfield,lpad(lastno,5,0)) AS docid from documenttypemappingdetails where orgid=?1 and finyear=?2 and branchcode=?3 and screencode=?4")
 	String getRCostInvoiceGnaDocId(Long orgId, String finYear, String branchCode, String screenCode);
 
-	@Query(nativeQuery = true, value = "SELECT * FROM partymaster WHERE partytype = ?1 AND active = 1")
+	@Query(nativeQuery = true, value = "SELECT * FROM finance.partymaster WHERE partytype = ?1 and gstregistered='YES'  AND active = 1")
 	List<PartyMasterVO> getPartyDetailsForRCostInvoice(String partyType);
 
 	@Query(value = "select a from PartyMasterVO a where a.orgId=?1 and a.partyType=?2 and a.active=true")
