f7808d7e0144fb76a12a41fed51ec025d6abcd65
narasimhawds <narashima@whydigit.in>
2025-03-25 19:14:10 +0530

    Error fixing

--- src/main/java/com/base/basesetup/controller/PartyTypeController.java
+++ src/main/java/com/base/basesetup/controller/PartyTypeController.java
@@ -414,8 +414,8 @@ public class PartyTypeController extends BaseController {
 	}
 
 	@GetMapping("/getAllPartyLedgerReport")
-	public ResponseEntity<ResponseDTO> getAllPartyLedgerReport(@RequestParam(required = false) Long orgId,@RequestParam(required = false) String partyName,
-			@RequestParam(required = false) String partyType, @RequestParam(required = false) String branch, String fromDate, String toDate) {
+	public ResponseEntity<ResponseDTO> getAllPartyLedgerReport(@RequestParam(required = true) Long orgId,@RequestParam(required = true) String partyName,
+			@RequestParam(required = true) String partyType, @RequestParam(required = true) String branch,@RequestParam(required = true) String fromDate,@RequestParam(required = true) String toDate) {
 		String methodName = "getAllPartyLedgerReport()";
 		LOGGER.debug(CommonConstant.STARTING_METHOD, methodName);
 		String errorMsg = null;
@@ -444,8 +444,8 @@ public class PartyTypeController extends BaseController {
 	
 	
 	@GetMapping("/getAllLedgerReport")
-	public ResponseEntity<ResponseDTO> getAllLedgerReport(@RequestParam(required = false) Long orgId,@RequestParam(required = false) String accountName,
-			@RequestParam(required = false) String branchCode,String fromDate, String toDate) {
+	public ResponseEntity<ResponseDTO> getAllLedgerReport(@RequestParam(required = true) Long orgId,@RequestParam(required = true) String accountName,
+			@RequestParam(required = true) String branchCode,@RequestParam(required = true) String fromDate,@RequestParam(required = true) String toDate) {
 		String methodName = "getAllLedgerReport()";
 		LOGGER.debug(CommonConstant.STARTING_METHOD, methodName);
 		String errorMsg = null;
--- src/main/java/com/base/basesetup/service/RCostInvoiceGnaServiceImpl.java
+++ src/main/java/com/base/basesetup/service/RCostInvoiceGnaServiceImpl.java
@@ -746,17 +746,18 @@ public class RCostInvoiceGnaServiceImpl implements RCostInvoiceGnaService {
 			map.put("branchCode", ch[0] != null ? ch[0].toString() : "");
 			map.put("vid", ch[1] != null ? ch[1].toString() : "");
 			map.put("vdate", ch[2] != null ? ch[2].toString() : "");
-//			map.put("supplierType", ch[3] != null ? ch[3].toString() : "");
 			map.put("supplierName", ch[3] != null ? ch[3].toString() : "");
-//			map.put("supplierBillNo", ch[4] != null ? ch[4].toString() : "");
 			map.put("supplierGstin", ch[4] != null ? ch[4].toString() : "");
-			map.put("gstType", ch[5] != null ? ch[5].toString() : "");
-			map.put("charges", ch[6] != null ? ch[6].toString() : "");
-			map.put("outputIgst", ch[7] != null ? ch[7].toString() : "");
-			map.put("outputCgst", ch[8] != null ? ch[8].toString() : "");
-			map.put("outputSgst", ch[9] != null ? ch[9].toString() : "");
-			map.put("gstPercent", ch[10] != null ? Double.valueOf(ch[10].toString()) : 0.0);
-
+			map.put("gstType", ch[5] != null ?  ch[5].toString() : "");
+			map.put("billAmount", ch[6] != null ?  new BigDecimal(ch[6].toString()) : BigDecimal.ZERO);
+			map.put("tax", ch[7] != null ? new BigDecimal(ch[7].toString()) : BigDecimal.ZERO);
+			map.put("totalAmount", ch[8] != null ? new BigDecimal(ch[8].toString()) : BigDecimal.ZERO);
+			map.put("tds", ch[9] != null ? new BigDecimal(ch[9].toString()) : BigDecimal.ZERO);
+			map.put("partyPayable", ch[10] != null ? new BigDecimal(ch[10].toString()) : BigDecimal.ZERO);
+			 map.put("outputIgst", ch[11] != null ? new BigDecimal(ch[11].toString()) : BigDecimal.ZERO);
+			 map.put("outputCgst", ch[12] != null ? new BigDecimal(ch[12].toString()) : BigDecimal.ZERO);
+			 map.put("outputSgst", ch[13] != null ? new BigDecimal(ch[13].toString()) : BigDecimal.ZERO);
+			 map.put("gstPercent", ch[14] != null ? new BigDecimal(ch[14].toString()) : BigDecimal.ZERO);
 			List1.add(map);
 		}
 		return List1;
