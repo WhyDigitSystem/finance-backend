40c414d3f74471b49c2a182572f64d747b8f2d5a
vasanthwhydigit <vasanth@whydigit.in>
2025-03-26 15:51:41 +0530

    report migration

--- src/main/java/com/base/basesetup/service/ReportService.java
+++ src/main/java/com/base/basesetup/service/ReportService.java
@@ -8,8 +8,11 @@ import org.springframework.stereotype.Service;
 
 import com.base.basesetup.dto.InvoiceDTO;
 import com.base.basesetup.dto.IssueManifestProviderDTO;
+import com.base.basesetup.dto.RetrievalManifestProviderDTO;
+import com.base.basesetup.entity.DeclarationAndNotesVO;
 import com.base.basesetup.entity.InvoiceVO;
 import com.base.basesetup.entity.IssueManifestProviderVO;
+import com.base.basesetup.entity.RetrievalManifestProviderVO;
 import com.base.basesetup.exception.ApplicationException;
 
 @Service
@@ -31,6 +34,20 @@ public interface ReportService {
 		Optional<IssueManifestProviderVO> getAllIssueManifestProviderById(Long id);
 
 		List<IssueManifestProviderVO> getAllIssueManifestProviderForPendingIssueRequest(Long orgId);
+		
+		//RETERIVELMANIFEST
+		
+	    Map<String, Object> createUpdateRetrievalManifest(RetrievalManifestProviderDTO retrievalManifestProviderDTO) throws ApplicationException;
+		
+		List<RetrievalManifestProviderVO> getAllRetrievalManifestProvider();
+		
+		Optional<RetrievalManifestProviderVO> getRetrievalManifestProviderById(Long id);
+		
+		//DECLARATION PART
+		
+		DeclarationAndNotesVO createDeclarationAndNotes(DeclarationAndNotesVO declarationAndNotesVO);
+
+		List<DeclarationAndNotesVO>  getAllDeclarationAndNotes();
 		
 	
 }
