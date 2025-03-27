9323bcddb289e9ac2bf8a828b850d9e7f815b6a4
richardwds <richard@whydigit.in>
2025-03-27 19:40:48 +0530

    arap ageing api

--- src/main/java/com/base/basesetup/entity/ScreenNamesVO.java
+++ src/main/java/com/base/basesetup/entity/ScreenNamesVO.java
@@ -29,7 +29,7 @@ public class ScreenNamesVO {
 	@Column(name = "screennameid")
 	private Long id;
 
-	@Column(name = "screenname", length = 25)
+	@Column(name = "screenname", length = 50)
 	private String screenName;
 
 	@Column(name = "screencode", length = 10)
